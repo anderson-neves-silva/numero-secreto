adc9605791f71ff462c45ae70796e194ff3405fb
Anderson Neves <andersonneves_@hotmail.com>
2022-10-21 11:33:19 -0300

    exibindo a mensagem falada na tela

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,19 +1,34 @@
-//aqui no início eu coloco window para que quando a página for aberta o reconhecimento de voz fique ativo
+//pegando o elemento chute
+const elementoChute = document.getElementById('chute');
+
+//aqui no início eu coloco window após a página aberta o reconhecimento de voz já fique ativo
 window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 
-//precisamos de uma instância para o funcionamento
+//precisamos de uma instância para o funcionamento do reconhecimento de voz
 const recognition = new SpeechRecognition();
 
-//agora sim vamos iniciar o reconhecimento de voz, e para isso precisamos configurar para a nossa lingua padrão português
+//agora sim vamos iniciar o reconhecimento de voz, e para isso devemos configurar para a nossa lingua padrão português
 recognition.lang = 'pt-Br';
 recognition.start();
 
-/*evento que axibe a mensagem no console, o comando abaixo significa quando o reconhecimento de voz iniciar eu quero pegar
+/*evento que axibe a mensagem no console, o comando abaixo significa, quando o reconhecimento de voz iniciar eu quero pegar
 tudo que está em "result" e será criada uma função callback para que ela seja executada*/
-recognition.addEventListener('result', onSpeak)
+recognition.addEventListener('result', onSpeak);
 
-/*essa função "onSpeak" exibe tudo que falamos no console, aqui passamos um parâmetro que é o evento, preciso pegar apenas o 
-result e exibir no console e para isso uso esse código "e.results[0][0].transcript"*/
+/*essa função "onSpeak" exibe tudo que falamos no console, aqui passamos um parâmetro "e" que é o evento, preciso pegar apenas o 
+results e exibir no console, e para isso eu uso esse código "e.results[0][0].transcript", agora crio uma variável que armazena 
+todo o elemento de fala o que mencionei antes, e chamo a função que exibe em tela passando o chute como parâmetro*/
 function onSpeak(e) {
-    console.log(e.results[0][0].transcript);
+    chute = (e.results[0][0].transcript);
+    //console.log(e.results[0][0].transcript);
+    exibeChuteNaTela(chute);    
+}
+
+/*função que exibe o chute na tela, ela recebe o parâmetro chute, mostra em tela a mensagem falada, Obs. usamos o "$" para acessar
+a variável chute, e aqui uso o sinal de crase*/
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>
+    `
 }
