1789bb7928a3b2602f6b40c028098f38aeb5d950
Anderson Neves <andersonneves_@hotmail.com>
2022-10-22 21:10:06 -0300

    exibindo as mensagens de resposta das validações direto na tela,  mudando o tamanho da fonte do h2 que mostra a mensagem quando a pessoa acertar o número secreto lá no arquivo css

--- app/validacao.js
+++ app/validacao.js
@@ -5,11 +5,21 @@ function verificaChuteSeEValido(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        console.log("Valor inválido");
+        // console.log("Valor inválido");
+        elementoChute.innerHTML += `<div>Valor inválido</div>`;
     }
 
     if (numeroMaiorMenorQueOPermitido(numero)) {
-        console.log(`Valor inválido: o número secreto precisa está entre ${menorValor} e ${maiorValor}`);
+        // console.log(`Valor inválido: o número secreto precisa está entre ${menorValor} e ${maiorValor}`);
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`;
+    }
+
+    // verifica se acertou o número secreto, se correto retiramos tudo o contúdo da tela e criamos um novo conteúdo lá
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Parabéns você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
     }
 }
 
